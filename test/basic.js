2a6ca48fec977a41567425404c0e6d953579d599
Ivo Georgiev <ivo@strem.io>
2018-04-14 19:11:04 +0300

    Finished linter

--- test/basic.js
+++ test/basic.js
@@ -5,14 +5,14 @@ const linter = require('../')
 tape('invalid manifest - undefined', function(t) {
 	let result = linter.lintManifest()
 	t.equal(result.valid, false, 'invalid manifest')
-	t.deepEqual(result.errors, [new Error('manifest should be an object')])
+	t.equal(result.errors[0].message, 'manifest should be an object')
 	t.end()
 })
 
 tape('invalid manifest - not an object', function(t) {
 	let result = linter.lintManifest('test')
 	t.equal(result.valid, false, 'invalid manifest')
-	t.deepEqual(result.errors, [new Error('manifest should be an object')])
+	t.equal(result.errors[0].message, 'manifest should be an object')
 	t.end()
 })
 
@@ -32,10 +32,28 @@ tape('invalid version', function(t) {
 		types: ['movie'],
 	})
 
-	t.equal(result.valid, false, 'valid manifest')
-	t.deepEqual(result.errors, [new Error('invalid version')])
+	t.equal(result.valid, false, 'invalid manifest')
+	t.equal(result.errors[0].message, 'manifest.version should be a valid semver version')
 	t.end()
 })
+
+tape('invalid catalogs', function(t) {
+	let result = linter.lintManifest({
+		id: 'org.myexampleaddon',
+		version: '1.0.0',
+		name: 'simple example',
+		resources: ['stream'],
+		types: ['movie'],
+		catalogs: '',
+		idPrefixes: '',
+	})
+
+	t.equal(result.valid, false, 'invalid manifest')
+	t.equal(result.errors[0].message, 'manifest.catalogs should be an array')
+	t.equal(result.errors[1].message, 'manifest.idPrefixes should be an array')
+	t.end()
+})
+
 
 tape('valid manifest', function(t) {
 	let result = linter.lintManifest({
