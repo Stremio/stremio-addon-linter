b29e15f933bf083fea6102d73455685d7cdee472
Ivo Georgiev <ivo@strem.io>
2018-04-14 19:31:54 +0300

    more tests, fixed a bug with catalogs

--- test/basic.js
+++ test/basic.js
@@ -37,7 +37,7 @@ tape('invalid version', function(t) {
 	t.end()
 })
 
-tape('invalid catalogs', function(t) {
+tape('invalid catalogs, idPrefixes', function(t) {
 	let result = linter.lintManifest({
 		id: 'org.myexampleaddon',
 		version: '1.0.0',
@@ -49,11 +49,28 @@ tape('invalid catalogs', function(t) {
 	})
 
 	t.equal(result.valid, false, 'invalid manifest')
+	t.equal(result.errors.length, 2, 'errors is right length')
 	t.equal(result.errors[0].message, 'manifest.catalogs should be an array')
 	t.equal(result.errors[1].message, 'manifest.idPrefixes should be an array')
 	t.end()
 })
 
+tape('invalid catalogs only', function(t) {
+	let result = linter.lintManifest({
+		id: 'org.myexampleaddon',
+		version: '1.0.0',
+		name: 'simple example',
+		resources: ['stream'],
+		types: ['movie'],
+		catalogs: [],
+		idPrefixes: '',
+	})
+
+	t.equal(result.valid, false, 'invalid manifest')
+	t.equal(result.errors.length, 1, 'errors is right length')
+	t.equal(result.errors[0].message, 'manifest.idPrefixes should be an array')
+	t.end()
+})
 
 tape('valid manifest', function(t) {
 	let result = linter.lintManifest({
@@ -62,6 +79,23 @@ tape('valid manifest', function(t) {
 		name: 'simple example',
 		resources: ['stream'],
 		types: ['movie'],
+	})
+
+	t.equal(result.valid, true, 'valid manifest')
+	t.deepEqual(result.errors, [], 'empty errors')
+	t.end()
+})
+
+
+tape('valid manifest with catalogs and idPrefixes', function(t) {
+	let result = linter.lintManifest({
+		id: 'org.myexampleaddon',
+		version: '1.0.0',
+		name: 'simple example',
+		resources: ['stream'],
+		types: ['movie'],
+		catalogs: [],
+		idPrefixes: [],
 	})
 
 	t.equal(result.valid, true, 'valid manifest')
