c6e54be1e52de288c8550a92d6f3ce34af6d1709
Ivo Georgiev <ivo@strem.io>
2018-05-27 15:27:40 +0300

    Collection linter

--- test/basic.js
+++ test/basic.js
@@ -172,3 +172,37 @@ tape('catalog validation - extraSupported/extraRequired', function(t) {
 	t.equal(result.errors[0].message, 'manifest.catalogs[0].extraSupported must be an array')
 	t.end()
 })
+
+tape('collection - not an array', function(t) {
+	let result = linter.lintCollection(false)
+
+	t.equal(result.valid, false, 'invalid collection')
+	t.equal(result.errors[0].message, 'col is not an array')
+	t.end()
+})
+
+tape('collection - invalid members', function(t) {
+	let result = linter.lintCollection([true, { }])
+
+	t.equal(result.valid, false, 'invalid collection')
+	t.equal(result.errors.length, 6, '6 errors') // 2x3
+	t.end()
+})
+
+tape('collection - valid collection', function(t) {
+	var manifest = {
+		id: 'org.myexampleaddon',
+		version: '1.0.0',
+		name: 'simple example',
+		resources: ['stream'],
+		types: ['movie'],
+		catalogs: [{ id: 'top', type: 'movie' }],
+		idPrefixes: [],
+	}
+	var col = [{ transportUrl: 'https://test.com', transportType: 'http', manifest: manifest }]
+
+	let result = linter.lintCollection(col)
+	t.equal(result.valid, true, 'valid collection')
+	t.equal(result.errors.length, 0, 'no errors')
+	t.end()
+})
